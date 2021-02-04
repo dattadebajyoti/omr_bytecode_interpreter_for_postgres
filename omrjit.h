72c6c85467b392131acb5145da3b85d44e70298b
dattadebajyoti <30590895+dattadebajyoti@users.noreply.github.com>
2021-02-03 23:10:52 -0400

    Update omrjit.h
    Added global vars for omr bytecode interpreter

--- omrjit.h
+++ omrjit.h
@@ -14,6 +14,9 @@ uint32 *off_opt;
 TupleDesc desc_opt;
 int natts_opt;
 bool is_compiled;
+bool b_is_compiled;
+bool m_is_compiled;
+bool h_is_compiled;
 int attnum_opt;
 bool is_virtual;
 bool agg_flag;
@@ -38,10 +41,17 @@ char	omrjit_path[MAXPGPATH];
 /*typedef int32_t (*omr_eval_compile)();
 omr_eval_compile omreval_compile;*/
 
-typedef void (OMRJIT_slot_deformFunctionType)(int32_t, TupleTableSlot *, HeapTuple , uint32 *);
-typedef OMRJIT_slot_deformFunctionType *(omr_eval_compile)();
+typedef void (OMRJIT_slot_deformFunctionType)(int32_t, TupleTableSlot *, HeapTuple , uint32 *, char *);
+typedef OMRJIT_slot_deformFunctionType *(omr_eval_compile)(int32_t , TupleDesc , TupleTableSlot */*, const TupleTableSlotOps **/);
 omr_eval_compile *omreval_compile;
 OMRJIT_slot_deformFunctionType *slot_deform;
+
+//can be deleted
+typedef void (OMRJIT_slot_deformFunctionType_hard_jit)(int32_t, TupleTableSlot *, HeapTuple , uint32 *, char *);
+typedef OMRJIT_slot_deformFunctionType_hard_jit *(omr_eval_compile_hard_jit)();
+omr_eval_compile_hard_jit *omreval_compile_hard_jit;
+OMRJIT_slot_deformFunctionType_hard_jit *slot_deform_hard_jit;
+//
 
 
 int32_t guaranteed_column_number;
@@ -55,8 +65,8 @@ typedef eval_expr_qual_FunctionType *(omr_expr_qual_compile)();
 omr_expr_qual_compile *expr_qual_compile;
 eval_expr_qual_FunctionType *qual_FunctionType;
 
-typedef int32_t (EEOP_FUNCEXPR_STRICT_FunctionType)(int32_t, NullableDatum *, bool **, Datum **, Datum, FunctionCallInfo);
-typedef EEOP_FUNCEXPR_STRICT_FunctionType *(omr_expr_FUNCEXPR_STRICT_compile)();
+typedef int32_t (EEOP_FUNCEXPR_STRICT_FunctionType)(bool **, Datum **, Datum, FunctionCallInfo);
+typedef EEOP_FUNCEXPR_STRICT_FunctionType *(omr_expr_FUNCEXPR_STRICT_compile)(int32_t, NullableDatum *);
 omr_expr_FUNCEXPR_STRICT_compile *expr_funcexprstrict_compile;
 EEOP_FUNCEXPR_STRICT_FunctionType *FUNCEXPR_STRICT;
 
@@ -97,9 +107,21 @@ EEOP_ASSIGN_TMP_MAKE_RO_FunctionType *EEOP_ASSIGN_TMP_MAKE_RO_Func;
 bool omr_jit_compile_expr;
 bool is_omr_expr_compiled;
 
+//compile time eval
+bool omr_byval[30];
+int16 omr_attlen[30];
+int indexer;
 
+//bytecode interpreter
+typedef Datum (omr_expression_bytecode_computation_FunctionType)(ExprState *, ExprContext *, bool *);
+typedef omr_expression_bytecode_computation_FunctionType *(omr_vm_compile)(ExprState *, ExprContext *, bool *);
+omr_vm_compile *omr_bytecode_Interpreter;
+omr_expression_bytecode_computation_FunctionType *omr_vm;
+//
 
-
+//new
+bool is_compile_FUNCEXPR_STRICT;
+//
 
 
 
